fc271125b3aa51bd184e095f3619bfb986bc3a1f
yassine_boujnah <yassineboujnah@gmail.com>
2020-04-14 16:34:22 +0100

    ajouter firestore

---
+++ app/src/main/java/com/ensias/healthcareapp/model/User.java
@@ -0,0 +1,13 @@
+package com.ensias.healthcareapp.model;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
