c11577a7715997cc74db62d477970eefbb5ca671
yassine_boujnah <yassineboujnah@gmail.com>
2020-06-02 14:47:03 +0100

    adding calendar Block

--- app/src/main/java/com/ensias/healthcareapp/SearchPatActivity.java
+++ app/src/main/java/com/ensias/healthcareapp/SearchPatActivity.java
@@ -1,6 +1,16 @@
 package com.ensias.healthcareapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.ensias.healthcareapp.adapter.DoctoreAdapter;
 import com.ensias.healthcareapp.model.Doctor;
@@ -10,6 +20,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,7 +34,7 @@ public class SearchPatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_pat);
-
+        configureToolbar();
         setUpRecyclerView();
 
     }
@@ -53,6 +64,37 @@ public class SearchPatActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation_bar, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setQueryHint(Html.fromHtml("<font color = #000000>" + "Search patient" + "</font>"));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+              //  adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        toolbar.setTitle("Doctors list");
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 
 
